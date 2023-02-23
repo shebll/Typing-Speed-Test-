8f4e12ed1c96511ecaddbad44e4b361c478cb1a6
shebl <shebblloll@gmail.com>
2023-02-23 08:30:06 +0200

    finish the project

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /////////////   [1] set random array of words  ///////////////////
 let randomString =
-  "Welcome to the website. If you're here, you're likely looking to find random words. Random Word Generator is the perfect tool to help you do this. While this tool isn't a word creator, it is a word generator that will generate random words for a variety of activities or uses. Even better, it allows you to adjust the parameters of the random words to best fit your needs";
+  "Welcome likely looking find perfect help do While this tool generator that will generate words for variety activities Even allows you adjust parameters of random best fit your needs";
 let randomWords = randomString.split(" ");
 // console.log(randomWords);
 ///////////////////////////////////////////////////////////////////
@@ -11,7 +11,7 @@ const lvl = {
   normal: 4,
   hard: 2,
 };
-let defaultLevelName = "normal";
+let defaultLevelName = "easy";
 let defaultLevelSeconds = lvl[defaultLevelName];
 // console.log(defaultLevelSeconds);
 ///////////////////////////////////////////////////////////////////
@@ -28,3 +28,76 @@ let score = document.querySelector(".game .control .score span.got");
 let totalScore = document.querySelector(".game .control .score span.total");
 let finish = document.querySelector(".game .finish");
 ///////////////////////////////////////////////////////////////////
+
+//////////////  [3] fill the dynamic data by js   //////////////
+levelName.innerHTML = defaultLevelName;
+levelSeconds.innerHTML = defaultLevelSeconds;
+secondsLeft.innerHTML = defaultLevelSeconds;
+totalScore.innerHTML = randomWords.length;
+score.innerHTML = 0;
+//////////////////////////////////////////////////////////////////
+
+//////////////  [5] remove paste event by js    //////////////////
+input.onpaste = () => {
+  return false;
+};
+//////////////////////////////////////////////////////////////////
+
+//////////////  [6] on start click functionalty    //////////////////
+startBtn.onclick = function () {
+  this.remove();
+  input.focus();
+  generate();
+};
+//////////////////////////////////////////////////////////////////
+
+//////////////  [7] make function that generate random array of word and put the data into html    //////////////////
+function generate() {
+  let randomNum = Math.trunc(Math.random() * randomWords.length);
+  let randomWord = randomWords[randomNum];
+  let indexWord = randomWords.indexOf(randomWord);
+  randomWords.splice(indexWord, 1);
+  theWord.innerHTML = randomWord;
+  upcomingWord.innerHTML = "";
+
+  for (let i = 0; i < randomWords.length; i++) {
+    let divWord = document.createElement("div");
+    divWord.appendChild(document.createTextNode(randomWords[i]));
+    upcomingWord.appendChild(divWord);
+  }
+  startPlay();
+}
+//////////////////////////////////////////////////////////////////
+
+//////////////  [8] make play function countdown  //////////////////
+function startPlay() {
+  secondsLeft.innerHTML = defaultLevelSeconds;
+  let startCount = setInterval(() => {
+    secondsLeft.innerHTML--;
+    if (input.value.toLowerCase() === theWord.innerHTML.toLowerCase()) {
+      console.log("good");
+      clearInterval(startCount);
+      input.value = "";
+      score.innerHTML++;
+      if (randomWords.length > 0) {
+        generate();
+      } else {
+        let span = document.createElement("span");
+        span.classList.add("good");
+        span.appendChild(document.createTextNode("good luck"));
+        finish.appendChild(span);
+      }
+    }
+    if (
+      secondsLeft.innerHTML == 0 &&
+      input.value.toLowerCase() != theWord.innerHTML.toLowerCase()
+    ) {
+      clearInterval(startCount);
+      let span = document.createElement("span");
+      span.classList.add("bad");
+      span.appendChild(document.createTextNode("bad luck"));
+      finish.appendChild(span);
+    }
+  }, 1000);
+}
+//////////////////////////////////////////////////////////////////
